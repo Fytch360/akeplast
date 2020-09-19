d924da9d26a20702a98e55501a854e861bccbac7
Fytch360 <58251278+Fytch360@users.noreply.github.com>
2020-09-19 21:00:40 +0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -61,3 +61,47 @@
   });
 
 })(jQuery); // End of use strict
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modall = document.querySelector(button.dataset.modalTarget); 
+    openModal(modall)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modall = button.closest('.modall')
+    closeModal(modall)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modall.active')
+  modals.forEach(modall => {
+    closeModal(modall) 
+  })
+})
+
+function openModal(modall) {
+  if (modall == null) return 
+  modall.classList.add('active')
+  overlay.classList.add('active')
+   
+}
+function closeModal(modall) {
+  if (modall == null) return 
+  modall.classList.remove('active')
+  overlay.classList.remove('active')
+   
+}
+
+
+
+
+
